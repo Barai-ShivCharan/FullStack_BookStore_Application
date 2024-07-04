aec0569212e4cd72dd024194c6d85bc5d2cf9fef
ShivCharan_Barai <barai104shivcharan@gmail.com>
2024-07-04 23:17:58 +0545

    Complete Fullstack BookStore Application:

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-30 items-center justify-center text-center p-20 mt-8">
+          <h1 className="text-2xl font-semibold md:text-4xl">
+            We are delighted to have you{" "}
+            <span className="text-pink-400">here!!!</span>
+          </h1>
+          <p className="m-12">
+            Purchasing a book is easy and rewarding! Browse our curated
+            selection, choose your favorite title, and enjoy seamless checkout.
+            Whether in-store or online, you'll quickly have a new book to dive
+            into and enjoy.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4 ">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
